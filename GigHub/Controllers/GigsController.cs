752c311e59f2a6ca8b6132d3459e3843d7c1d614
Aaslam89 <AmirAslam003@yahoo.com>
2016-06-21 13:23:29 -0400

    first commit

---
+++ GigHub/Controllers/GigsController.cs
@@ -0,0 +1,101 @@
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [Authorize]
+        public ActionResult Mine()
+        {
+            var userId = User.Identity.GetUserId();
+            var gigs = _context.Gigs
+                .Where(x => x.ArtistId == userId && x.DateTime > DateTime.Now)
+                .Include(x => x.Genre)
+                .ToList();
+
+            return View(gigs);
+        }
+
+        [Authorize]
+        public ActionResult Attending()
+        {
+            var userId = User.Identity.GetUserId();
+            var gigs = _context.Attendances
+                .Where(x => x.AttendeeId == userId)
+                .Select(x=>x.Gig)
+                .Include(x=>x.Artist)
+                .Include(x=>x.Genre)
+                .ToList();
+
+            var viewModel = new GigsViewModel()
+            {
+                UpComingGigs = gigs,
+                ShowActions = User.Identity.IsAuthenticated,
+                Heading = "Gigs I'm Attending"
+            };
+            return View("Gigs", viewModel);
+        }
+        // GET: Gigs
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel()
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var gig = _context.Gigs.Single(x => x.Id == id && x.ArtistId == userId);
+
+            var viewModel = new GigFormViewModel()
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(GigFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.Genres = _context.Genres.ToList();
+                return View("Create", model);
+            }
+            else
+            {
+                var gig = new Gig()
+                {
+                    ArtistId = User.Identity.GetUserId(),
+                    DateTime = model.GetDateTime(),
+                    GenreId = model.Genre,
+                    Venue = model.Venue
+                };
+
+                _context.Gigs.Add(gig);
+                _context.SaveChanges();
+                return RedirectToAction("Mine", "Gigs");
+            }           
+        }
+    }
+}
---
+++ GigHub/Migrations/201606091525466_PopulateGenresTable.cs
@@ -0,0 +1,21 @@
+namespace GigHub.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class PopulateGenresTable : DbMigration
+    {
+        public override void Up()
+        {
+            
+            Sql("INSERT INTO Genres (Name) Values ('Jazz')");
+            Sql("INSERT INTO Genres (Name) Values ('Blues')");
+            Sql("INSERT INTO Genres (Name) Values ('Rock')");
+            Sql("INSERT INTO Genres (Name) Values ('Country')");
+        }
+
+        public override void Down()
+        {
+            
+        }
+    }
+}
